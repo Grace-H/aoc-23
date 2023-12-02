9ea7a6f91cbb506027ff5445a586ed77a4f2bdbe
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-12-01 23:30:57 -0600

    day 2 b

--- day2.py
+++ day2.py
@@ -1,35 +1,23 @@
 #!/usr/bin/env python3
 
 filename = "input2"
-# filename = "test2"
+#filename = "test2"
 file = open(filename, 'r')
 lines = file.readlines() #[l.split(" ") for l in file.readlines()]
 file.close()
 
-red = 0
-blue = 0
-green = 0
-
-m = {"red": 12, "green": 13, "blue": 14}
-redt = 12
-greent = 13
-bluet = 14
 total = 0
 for l in lines:
     words = l.split(':')
     game = int(words[0].split(' ')[1].strip(':'))
     d = words[1].split(';')
-    valid = True
+    mm = {"red": [], "green": [], "blue": []}
     for g in d:
         colors = g.split(',')
         for c in colors:
             ms = c.strip().split(' ')
-            print(ms)
-            if m[ms[1].strip()] < int(ms[0].strip()):
-                print("indvalid", ms)
-                valid = False
-    if valid:
-        total += game
-        print(game)
+            mm[ms[1].strip()].append(int(ms[0].strip()))
 
+    total += max(mm["red"])*max(mm["green"])*max(mm["blue"]) 
+    
 print(total)
