c3b3dd1a4c34d46a8f65b38dea40a5397bac4a5a
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-12-03 00:13:06 -0600

    make a few things easier

--- starter.py
+++ starter.py
@@ -7,15 +7,17 @@ def strparse(lines):
     p = compile("Game {:d}: {}\n")
     return map(getattr, map(p.parse, lines), repeat('fixed'))
 
-filename = "input3"
-# filename = "test3"
-file = open('input3', 'r')
+filename = "input4"
+# filename = "test4"
+file = open(filename, 'r')
 lines = file.readlines() #[l.split(" ") for l in file.readlines()]
 file.close()
 
 lines = strparse(lines)
 print(lines)
+
+nums = []
 for l in lines:
     print(i[1])
 
-# sum(sort(out)[0:3])
+print(sum(nums))
