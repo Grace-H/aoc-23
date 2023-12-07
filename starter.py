ae3a0f5297802e0e6409182466156e41f3deb891
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-12-07 10:54:00 -0600

    more imports, simplify starter

--- starter.py
+++ starter.py
@@ -2,22 +2,21 @@
 
 from parse import compile
 from itertools import repeat
+from collections import Counter
+from functools import reduce
+from operator import mul, concat
 
 def strparse(lines):
     p = compile("Game {:d}: {}\n")
     return map(getattr, map(p.parse, lines), repeat('fixed'))
 
-filename = "input4"
-# filename = "test4"
+filename = "input8"
+# filename = "test8"
 file = open(filename, 'r')
 lines = file.readlines() #[l.split(" ") for l in file.readlines()]
 file.close()
 
-lines = strparse(lines)
 print(lines)
 
-nums = []
 for l in lines:
-    print(i[1])
-
-print(sum(nums))
+    print(l)
